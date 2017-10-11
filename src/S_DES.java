831c5534c590c2d83b938067ab5d83bd0db1af4b
Sylvester Zowonu <szowonu@hotmail.com>
2017-10-10 22:21:05 -0700

    ...

--- src/S_DES.java
+++ src/S_DES.java
@@ -1,24 +1,54 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class S_DES {
 	// S-Boxes
     final static int[][] S0 = { {1,0,3,2} , {3,2,1,0} , {0,2,1,3} , {3,1,3,2} } ;
     final static int[][] S1 = { {0,1,2,3},  {2,0,1,3}, {3,0,1,0}, {2,1,0,3}} ;
-	public static byte key1 [] = {1, 1, 1, 0, 0, 0, 1, 1, 1, 0};
- 	
+	public static byte key [] = {1, 1, 1, 0, 0, 0, 1, 1, 1, 0};
+	Scanner input = new Scanner(System.in);
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Key : "+Arrays.toString(key1));
+		
+		S_DES S_DES = new S_DES();
+		System.out.println("Key : "+Arrays.toString(key));
+		//genKeys();
+		int response = 0;
+		do {
+			response = S_DES.mainMenu();
+			switch(response){
+				case 0:
+					System.out.print("Enter KEY : ");
+					
+					System.out.print("Enter Plaintext : ");
+										
+					break;
+				case 1:
+					System.out.print("Enter KEY : ");
+					
+					System.out.print("Enter Ciphertext : ");
+					
+					break;
+				case 2:
+					System.out.println("Exiting Program...!");
+					break;
+				default:
+					System.out.println("Invalid Entry...Please check and try again!!!");
+					break;
+			}
+		}while(response!=0);
 		System.out.print(Arrays.toString(S0));
 	}
-	public static byte[] Encrypt(byte[] rawkey, byte[] plaintext) {
+	public byte[] Encrypt(byte[] rawkey, byte[] plaintext) {
 		return null;
 	}
-	public static byte[] Decrypt(byte[] rawkey, byte[] ciphertext) {
+	public byte[] Decrypt(byte[] rawkey, byte[] ciphertext) {
 		return null;
 	}
-	
-	public byte [] Perm10(byte k1[]) {
+	public void genKeys() {
+		
+	}
+	public byte [] P10(byte k1[]) {
 		/** Perform permutation P10 on 10-bit key 
 		  Perm10(k1, k2, k3, k4, k5, k6, k7, k8, k9, k10)  
 		        (k3, k5, k2, k7, k4, k10, k1, k9, k8, k6)
@@ -37,7 +67,18 @@ public class S_DES {
 		return temp;
 	}
 	
-	public byte [] Perm8(byte k1[]) {
+	public byte [] P8(byte k1[]) {
+		
 		return null;
+	}
+	public int mainMenu() {
+		int s = 0;
+		System.out.println("-------------------------------------");
+		System.out.println("Please follow instructions below!\nEnter the corresponding number");
+		System.out.println("1.\tEncrypt Text");
+		System.out.println("2.\tDecrypt Text");
+		System.out.println("3.\tExit S-DES");
+		s = input.nextInt();
+		return s;
 	}
 }
